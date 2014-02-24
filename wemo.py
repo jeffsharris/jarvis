e26b7c9adda8ef17a3a831bb123bb1fa4fabff1d
JSH <jeff.s.harris@gmail.com>
2014-02-23 21:57:30 -0800

    Add a wemo controller that turns off the switch after 30 minutes of inactivity

---
+++ wemo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import time
+from ouimeaux.environment import Environment
+from ouimeaux.signals import statechange, receiver
+
+timeoutTime = 1800 # Wait 30 minutes without motion before turning off the volcano
+waitTime = 60 # Check if it has timed out every 60 seconds 
+	
+env = Environment()
+env.start()
+
+switch = env.get_switch('Volcano')
+motion = env.get_motion('Living Room')
+
+lastMovedTime = time.time()
+
+@receiver(statechange, sender=motion)
+def motion_detected(state, sender, signal):
+	global lastMovedTime
+	if (state == 1):
+		print "Motion detected"
+		lastMovedTime = time.time()
+		
+@receiver(statechange, sender=switch)
+def switch_detected(state, sender, signal):
+	global lastMovedTime
+	if (state == 1):
+		print "Switch detected"
+		lastMovedTime = time.time()
+	
+
+print "Waiting for events"
+while True:
+	env.wait(waitTime)
+	print "Switch state is: ", switch.basicevent.GetBinaryState()['BinaryState']
+	if (time.time() - lastMovedTime > timeoutTime) & (switch.basicevent.GetBinaryState()['BinaryState'] == "1"):
+		print "Turning switch off"
+		switch.basicevent.SetBinaryState(BinaryState=0)
