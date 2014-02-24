6f485f9fdd2949d58b9b13ce2b512bdd4ce3baff
Jeff Harris <jeff.s.harris@gmail.com>
2014-02-23 22:09:49 -0800

    Update the controller to start the wemo listener

--- controller.py
+++ controller.py
@@ -3,7 +3,9 @@
 import physicalcontroller
 import server
 import threading
+import wemo
 
 if __name__ == "__main__":
 	threading.Thread(target=physicalcontroller.start).start()
 	threading.Thread(target=server.start).start()
+	threading.Thread(target=wemo.start).start()
