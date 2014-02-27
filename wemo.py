04c006835dd11ba7743ca24d4bad3cbae8ac6866
Jeff Harris <jeff.s.harris@gmail.com>
2014-02-27 00:05:34 -0800

    Fix bugs with restarting switch if motion is detected soon after shutdown

--- wemo.py
+++ wemo.py
@@ -5,12 +5,18 @@ from ouimeaux.environment import Environment
 from ouimeaux.signals import statechange, receiver
 
 timeoutTime = 1800 # Wait 30 minutes without motion before turning off the volcano
-reactivateTime = 300 # If motion is detected within 5 minutes of turning off, then turn on again
+reactivateTime = 300 # If motion is detected within 5 minutes of turning off, then turn on again. Must be less than timeoutTime
 waitTime = 60 # Check if it has timed out every 60 seconds 
-	
-env = Environment()
-env.discover(seconds=3)
+
+def on_motion(motion):
+	print "Motion found!", motion.name
+
+def on_switch(switch):
+	print "Switch found!", switch.name
+
+env = Environment(on_switch, on_motion)
 env.start()
+env.discover(seconds=3)
 
 switch = env.get_switch('Volcano')
 motion = env.get_motion('Living Room')
@@ -35,17 +41,16 @@ def switch_detected(state, sender, signal):
 		timedOut = False
 		
 
-if __name__ == "__main__":
-	print "Waiting for events"
-	while True:
-		env.wait(waitTime)
-		print "Switch state is: ", switch.basicevent.GetBinaryState()['BinaryState']
-		if ((time.time() - lastMovedTime) > timeoutTime) & (switch.basicevent.GetBinaryState()['BinaryState'] == "1"):
-			print "Turning switch off"
-			switch.basicevent.SetBinaryState(BinaryState=0)
-			timedOut = True
+print "Waiting for events"
+while True:
+	env.wait(waitTime)
+	print "Switch state is: ", switch.basicevent.GetBinaryState()['BinaryState']
+	if ((time.time() - lastMovedTime) > timeoutTime) & (switch.basicevent.GetBinaryState()['BinaryState'] == "1"):
+		print "Turning switch off"
+		switch.basicevent.SetBinaryState(BinaryState=0)
+		timedOut = True
 		
-		if timedOut & ((time.time() - lastMovedTime) < reactivateTime):
-			print "Turning switch back on"
-			switch.basicevent.SetBinaryState(BinaryState=1)
-			timedOut = False
+	if timedOut & ((time.time() - lastMovedTime) < reactivateTime):
+		print "Turning switch back on"
+		switch.basicevent.SetBinaryState(BinaryState=1)
+		timedOut = False
